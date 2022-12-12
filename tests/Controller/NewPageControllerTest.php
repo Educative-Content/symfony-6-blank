2c44611a3642ed3dd7f24805305398e6d5edc7ac
asrindayananda <adayanan@deakin.edu.au>
2022-12-12 08:55:48 +0000

    Add a unit test

---
+++ tests/Controller/NewPageControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class NewPageControllerTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertEquals(1,1);
+    }
+}
