011969709b7f753e0624b1bb8e527a8597cf0115
asrindayananda <adayanan@deakin.edu.au>
2023-02-04 14:56:31 +0000

    Blank NewPageController

--- src/Controller/NewPageController.php
+++ src/Controller/NewPageController.php
@@ -1,18 +1,3 @@
 <?php
 
-namespace App\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-
-class NewPageController extends AbstractController
-{
-    #[Route('/new_page', name: 'new_page')]
-    public function index(): Response
-    {
-        return new Response(
-            '<html><body>Hello welcome to new page</body></html>'
-        );
-    }
-}
+//Add your code here
