79c915de4b6a624453255a77868efbca258623a9
asrindayananda <asrindayananda@gmail.com>
2023-04-01 15:52:01 +0100

    Remove default class

--- tests/Controller/NewPageControllerTest.php
+++ tests/Controller/NewPageControllerTest.php
@@ -1,13 +1,1 @@
 <?php
-
-namespace App\Tests;
-
-use PHPUnit\Framework\TestCase;
-
-class NewPageControllerTest extends TestCase
-{
-    public function testSomething()
-    {
-        $this->assertEquals(1,1);
-    }
-}
